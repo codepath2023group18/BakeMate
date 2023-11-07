7db16e16690b3083b34cc3021f9174ed5efa32f9
Adrian Ea <106501057+GreyActOwl1@users.noreply.github.com>
2023-11-07 17:31:01 -0500

    Upgrade compileSdk from 33 to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.bakemate"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.bakemate"
@@ -44,4 +44,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.codepath.libraries:asynchttpclient:2.2.0")
 }
