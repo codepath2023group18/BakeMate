aaf9443a37a2933c18c22d9e40775f4aab9e58a6
Adrian Ea <106501057+GreyActOwl1@users.noreply.github.com>
2023-11-10 09:49:44 -0500

    Add parsing to JsonHttpResponseHandler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.codepath.libraries:asynchttpclient:2.2.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+
+
 }
--- app/src/main/java/com/example/bakemate/MainActivity.kt
+++ app/src/main/java/com/example/bakemate/MainActivity.kt
@@ -6,6 +6,9 @@ import android.util.Log
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,18 +28,37 @@ class MainActivity : AppCompatActivity() {
                 json: JsonHttpResponseHandler.JSON
             ) {
                 // Access a JSON object response with `json.jsonObject`
-                 Log.d("DEBUG OBJECT", json.jsonObject.toString())
+                Log.d("OBJECT", json.jsonObject.toString())
                 //Log.d("DEBUG RESPONSE", json.jsonArray.toString())
                 // Log.d("DEBUG RESPONSE", "$headers")
                 val rateLimitUsed = headers["x-ratelimit-used"]
                 val rateLimitRemaining = headers["x-ratelimit-remaining"]
                 val rateLimitReset = headers["x-ratelimit-reset"]
                 Log.d(
-                    "DEBUG RESPONSE",
+                    "RESPONSE",
                     " rate used: $rateLimitUsed |rate limit remaining: $rateLimitRemaining" +
                             "| rate limit reset (seconds) $rateLimitReset"
                 )
-                Log.d("DEBUG RESPONSE", "$json")
+                Log.d("RESPONSE", "$json")
+
+                val jsonString = json.jsonObject.toString()
+                val jsonObject = Json.parseToJsonElement(jsonString)
+                val dataObject = jsonObject?.jsonObject?.get("data")
+                val childrenArray = dataObject?.jsonObject?.get("children")?.jsonArray
+
+                if (childrenArray != null) {
+                    for (childJsonObj in childrenArray) {
+                        if (childJsonObj is JsonObject) {
+                            val childDataObj = childJsonObj["data"]?.jsonObject
+                            val title = childDataObj?.get("title")?.jsonPrimitive?.content
+                            val url = childDataObj?.get("url")?.jsonPrimitive?.content
+
+                            Log.d("Parsing Json", "title: $title  | imageURL $url")
+//                            TODO("Filter text only urls")
+//                            TODO("Save valid text, url pairs for use in activity views")
+                        }
+                    }
+                }
             }
 
             override fun onFailure(
